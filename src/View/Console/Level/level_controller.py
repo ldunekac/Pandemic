acc478750b806f2c5a0ccdf9c3dffe95aaa536e9
Chris Loew <cloew123@gmail.com>
2013-06-22 14:21:25 -0600

    Added a stubbed out level view

--- src/View/Console/Level/level_controller.py
+++ src/View/Console/Level/level_controller.py
@@ -1,13 +1,15 @@
 from Level.level import Level
 
+from View.Console.Level.level_view import LevelView
+
 class LevelController:
     """ Controller for the level """
     
     def __init__(self):
         """ Initialize the Level Controller """
         self.level = Level()
-        self.screen = None
+        self.screen = LevelView(self.level)
         
     def run(self):
         """ Run this controller """
-        self.screen.print()
+        self.screen.display()
---
+++ src/View/Console/Level/level_view.py
@@ -0,0 +1,11 @@
+
+class LevelView:
+    """ Represents the view of the Level """
+    
+    def __init__(self, level):
+        """ Initialize the Level View with its level """
+        self.level = level
+        
+    def display(self):
+        """ Display the Level View """
+        print ""
