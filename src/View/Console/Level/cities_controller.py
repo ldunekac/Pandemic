95d8bb1abdb905f8f1c29a2b87954306f078eba9
Chris Loew <cloew123@gmail.com>
2013-06-22 15:53:22 -0600

    Cleaned up the Cities Controller and View

--- src/View/Console/Level/cities_controller.py
+++ src/View/Console/Level/cities_controller.py
@@ -9,9 +9,63 @@ class CitiesController(Controller):
         """ Initialize the Cities Controller """
         Controller.__init__(self, CitiesView(cities))
         
-        self.addCommand(ord('1'), self.viewCity)
+        self.addCommand(ord('0'), self.viewCityZero)
+        self.addCommand(ord('1'), self.viewCityOne)
+        self.addCommand(ord('2'), self.viewCityTwo)
+        self.addCommand(ord('3'), self.viewCityThree)
+        self.addCommand(ord('4'), self.viewCityFour)
+        self.addCommand(ord('5'), self.viewCityFive)
+        self.addCommand(ord('6'), self.viewCitySix)
+        self.addCommand(ord('7'), self.viewCitySeven)
+        self.addCommand(ord('8'), self.viewCityEight)
+        self.addCommand(ord('9'), self.viewCityNine)
         
-    def viewCity(self):
+        self.addCommand(ord('n'), self.screen.selectNextSection)
+        self.addCommand(ord('p'), self.screen.selectPreviousSection)
+        
+    def viewCityZero(self):
         """ View the selected city """
-        controller = CityController(self.screen.cities[0])
-        controller.run()
+        self.viewCity(0)
+        
+    def viewCityOne(self):
+        """ View the selected city """
+        self.viewCity(1)
+        
+    def viewCityTwo(self):
+        """ View the selected city """
+        self.viewCity(2)
+        
+    def viewCityThree(self):
+        """ View the selected city """
+        self.viewCity(3)
+        
+    def viewCityFour(self):
+        """ View the selected city """
+        self.viewCity(4)
+        
+    def viewCityFive(self):
+        """ View the selected city """
+        self.viewCity(5)
+        
+    def viewCitySix(self):
+        """ View the selected city """
+        self.viewCity(6)
+        
+    def viewCitySeven(self):
+        """ View the selected city """
+        self.viewCity(7)
+        
+    def viewCityEight(self):
+        """ View the selected city """
+        self.viewCity(8)
+        
+    def viewCityNine(self):
+        """ View the selected city """
+        self.viewCity(9)
+        
+    def viewCity(self, index):
+        """ View the selected city """
+        city = self.screen.getCity(index)
+        if city is not None:
+            controller = CityController(city)
+            controller.run()
--- src/View/Console/Level/cities_view.py
+++ src/View/Console/Level/cities_view.py
@@ -1,15 +1,52 @@
 
 class CitiesView:
     """ Represents the view of the cities """
+    MAX_CITIES = 10
     
     def __init__(self, cities):
         """ Initialize the Cities View """
         self.cities = cities
         
+        self.section = 0
+        
     def display(self):
         """ Display all the cities """
-        print "Cities", "\r"
+        self.displayHeader()
+        
+        value = 0
+        for city in self.cities[self.section*CitiesView.MAX_CITIES:self.section*CitiesView.MAX_CITIES+CitiesView.MAX_CITIES]:
+            print "{0}: {1}\r".format(value, city)
+            value += 1
+            
+    def displayHeader(self):
+        """ Disaply Header Information """
+        print "Cities:", len(self.cities), "\r"
+        
+        start = self.section*CitiesView.MAX_CITIES
+        end = self.section*CitiesView.MAX_CITIES+CitiesView.MAX_CITIES-1
+        
+        if end > len(self.cities):
+            end = len(self.cities)-1
+        
+        print "Showing Cities: {0}-{1}\r".format(start, end)
+        if end < len(self.cities)-1:
+            print "n: Show Next Section\r"
+        if self.section > 0:
+            print "p: Show Previous Section\r"
         print
         
-        for city in self.cities:
-            print city, "\r"
+    def getCity(self, index):
+        """ Return the city in the current section with the given index """
+        actualIndex = self.section*CitiesView.MAX_CITIES + index
+        if actualIndex < len(self.cities):
+            return self.cities[actualIndex]
+        
+    def selectNextSection(self):
+        """ Select the next section """ 
+        if self.section < len(self.cities)%CitiesView.MAX_CITIES:
+            self.section += 1
+        
+    def selectPreviousSection(self):
+        """ Select the previous section """ 
+        if self.section > 0:
+            self.section -= 1
