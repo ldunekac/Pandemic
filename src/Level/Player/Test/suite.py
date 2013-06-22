35c45e0bde3ba06f4d898654cc2ddce83f63cfa3
Luke <ldunekacke@gmail.com>
2013-06-22 16:55:46 -0600

    added functionally to player class along with passing tests

--- src/Level/Player/Test/suite.py
+++ src/Level/Player/Test/suite.py
@@ -1,6 +1,6 @@
 import unittest
 
+from player_test import suite as palyer_suite
 
-
-suites = []
+suites = [palyer_suite]
 suite = unittest.TestSuite(suites)
--- src/Level/Player/player.py
+++ src/Level/Player/player.py
@@ -1,6 +1,17 @@
-
+from Level.city import City
 
 class Player:
 
-    def __init__(self):
+    DISEASE_CURE_AMOUNT = 1
+
+    def __init__(self, startingCity):
         """ initializes player class"""
+        self.city = startingCity
+
+    def moveTo(self, city):
+        """Move player to city"""
+        self.city = city
+
+    def cureDisease(self, disease = None):
+        """Cures a disease in their current city"""
+        self.city.cureDisease(self.DISEASE_CURE_AMOUNT,disease)
