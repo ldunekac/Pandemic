2a86ac0f7650504897c55289986a1323709a2020
Luke <ldunekacke@gmail.com>
2013-06-22 13:11:46 -0600

    added the deck class along with testing suite

---
+++ src/Level/Deck/deck.py
@@ -0,0 +1,23 @@
+import random
+
+class Deck:
+
+    """"Sets up a deck"""
+    def __init__(self, deck):
+        self.deck = deck
+        self.discardPile = []
+        self.shuffle()
+
+    def  shuffle(self):
+        """ Shuffles the deck"""
+        random.shuffle(self.deck)
+
+    def draw(self):
+        if len(self.deck) == 0:
+            return None
+        card = self.deck.pop()
+        return card
+
+    def discard(self, card):
+        """Adds a card to the discard pile"""
+        self.discardPile.append(card)
--- src/Level/Test/suite.py
+++ src/Level/Test/suite.py
@@ -1,7 +1,7 @@
 import unittest
 
 from city_test import suite as city_test_suite
+from Level.Deck.Test.suite import suite as deck_suite
 
-
-suites = [city_test_suite]
+suites = [city_test_suite, deck_suite]
 suite = unittest.TestSuite(suites)
