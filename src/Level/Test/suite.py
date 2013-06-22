d242ea4400b59bbbf61611dd640e1d87d999ae24
Luke <ldunekacke@gmail.com>
2013-06-22 15:42:39 -0600

    added player and test files

--- src/Level/Test/suite.py
+++ src/Level/Test/suite.py
@@ -4,8 +4,10 @@ from city_test import suite as city_test_suite
 from level_test import suite as level_test_suite
 
 from Level.Deck.Test.suite import suite as deck_suite
+from Level.Player.Test.suite import suite as player_suite
 
 suites = [city_test_suite, 
           deck_suite,
-          level_test_suite]
+          level_test_suite,
+          player_suite]
 suite = unittest.TestSuite(suites)
