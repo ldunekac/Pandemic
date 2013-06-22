d0275041450349be0bfcd342a2cf82f7486a0279
Chris Loew <cloew123@gmail.com>
2013-06-22 11:58:07 -0600

    Added a stubbed out disease class

---
+++ src/Level/Disease/disease.py
@@ -0,0 +1,8 @@
+
+class Disease:
+    """ Represents a disease in a level of the game """
+    
+    def __init__(self):
+        self.cubeCount = 24
+        self.cured = False
+        self.eradicated = False
