e050819ece333ec652d6047311186c36c29b21df
Chris Loew <cloew123@gmail.com>
2013-06-22 14:58:01 -0600

    Added a stubbed out cities controller

---
+++ src/View/Console/Level/cities_controller.py
@@ -0,0 +1,8 @@
+from View.Console.controller import Controller
+
+class CitiesController(Controller):
+    """ Controller for cities list """
+
+    def __init__(self):
+        """ Initialize the Cities Controller """
+        Controller.__init__(self, None)
