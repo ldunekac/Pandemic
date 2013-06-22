a0d8e01d7d5686d6937b09e8dbb7e32323cbab60
Luke <ldunekacke@gmail.com>
2013-06-22 14:05:15 -0600

    deck tests added

--- src/Level/Deck/Test/deck_test.py
+++ src/Level/Deck/Test/deck_test.py
@@ -6,15 +6,31 @@ class draw(unittest.TestCase):
     
     def  setUp(self):
         """ Build the *** for the test """
-        self.testDeck = Deck([99])
+        self.testDeck = Deck([])
         
+    def makeOneElementDeck(self):
+        self.testDeck = Deck([99])
+
+    def makeTwoElementDeck(self):
+        self.testDeck = Deck([1,2])
+
     def testOneCard(self):
         """ Test that ... """
         self.testDeck.draw()
-        assert(len(self.testDeck.deck) == None, "Test deck of one element did not remove a card when drawned")
+        assert len(self.testDeck.deck) == 0, "Test deck of one element did not remove a card when drawned"
+
+    def testSeveralCards(self):
+        self.makeTwoElementDeck()
+        card = self.testDeck.draw()
+        #print card
+        assert (card == 2), "Card given is not the last card in the deck"
+
+    def testDiscard(self):
+        self.testDeck.discard(7)
+        assert 7 in self.testDeck.discardPile, "Card was not inserted into the discard list"
 
 # Collect all test cases in this class
-testcasesDraw = ["testOneCard"]
+testcasesDraw = ["testOneCard", "testSeveralCards", "testDiscard"]
 suiteDraw = unittest.TestSuite(map(draw, testcasesDraw))
 
 ##########################################################
