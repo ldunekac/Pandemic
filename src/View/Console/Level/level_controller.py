08c1def2c40ac00a7153413212864e0ab96eb020
Chris Loew <cloew123@gmail.com>
2013-06-22 17:07:54 -0600

    Can now do player related actions

--- src/View/Console/Level/level_controller.py
+++ src/View/Console/Level/level_controller.py
@@ -2,6 +2,7 @@ from Level.level import Level
 
 from View.Console.controller import Controller
 from View.Console.Level.cities_controller import CitiesController
+from View.Console.Level.city_controller import CityController
 from View.Console.Level.level_view import LevelView
 
 class LevelController(Controller):
@@ -11,10 +12,25 @@ class LevelController(Controller):
         """ Initialize the Level Controller """
         self.level = Level()
         Controller.__init__(self, LevelView(self.level))
-        self.addCommand(ord('1'), self.viewCities)
+        self.addCommand(ord('1'), self.viewCity)
+        self.addCommand(ord('2'), self.viewCities)
+        self.addCommand(ord('3'), self.moveToACity)
+        self.addCommand(ord('4'), self.cureCurrentCity)
+        
+    def viewCity(self):
+        """ View the level's cities """
+        controller = CityController(self.level.players[0].city)
+        controller.run()
             
     def viewCities(self):
         """ View the level's cities """
         controller = CitiesController(self.level.cities)
         controller.run()
         
+    def moveToACity(self):
+        """ Move the Player to a city """
+        self.level.players[0].moveTo(self.level.cities[0])
+        
+    def cureCurrentCity(self):
+        """ Cure the current city """
+        self.level.players[0].cureDisease()
--- src/View/Console/Level/level_view.py
+++ src/View/Console/Level/level_view.py
@@ -9,10 +9,13 @@ class LevelView:
     def display(self):
         """ Display the Level View """
         self.displayPlayerDetails(self.level.players)
-        print "1. View cities\r"
-        # User actions
+        print "1. View Current City\r"
+        print "2. View Cities\r"
+        print "3. Move City\r"
+        print "4. Cure Current City\r"
         
     def displayPlayerDetails(self, players):
         """ Display the current player """
         for player in players:
-            print "Player: {0}".format(player.city)
+            print "Player: {0}\r".format(player.city)
+        print
