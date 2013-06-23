b0d010d5a97344f29f438ff066810bb5af4890f1
Chris Loew <cloew123@gmail.com>
2013-06-22 20:08:07 -0600

    Player can now move around the bored

--- src/View/Console/Level/Player/player_move_view.py
+++ src/View/Console/Level/Player/player_move_view.py
@@ -5,6 +5,7 @@ class PlayerMoveView:
     def __init__(self, player):
         """ Initialize the Player Move View """
         self.player = player
+        self.cities = list(player.city.adjacentCities)
         
     def display(self):
         """ Display the Player Move Options """
@@ -12,6 +13,13 @@ class PlayerMoveView:
         print
         
         value = 0
-        for city in self.player.city.adjacentCities:
+        for city in self.cities:
             print "{0}: {1}\r".format(value, city)
             value += 1
+            
+    def getCity(self, index):
+        """ Return the city at the given index """
+        if index < len(self.cities):
+            return self.cities[index]
+        return None
+        
