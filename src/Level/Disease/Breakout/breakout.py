a6f78143b69ee76e91f7af492777a1182ebc33b1
Chris Loew <cloew123@gmail.com>
2013-07-09 13:08:52 -0600

    Added a Breakout Class with a basic breakout loop

---
+++ src/Level/Disease/Breakout/breakout.py
@@ -0,0 +1,13 @@
+
+class Breakout:
+    """ Represents a disease breakout in a given city """
+    
+    def __init__(self, startingCity):
+        """ Initialize the Breakout """
+        self.startingCity = startingCity
+        
+    def breakout(self):
+        """ Have the breakout occur in the Starting City """
+        for city in self.startingCity.adjacentCities:
+            # Check if city has already been hit by the breakout
+            city.infect(1)
