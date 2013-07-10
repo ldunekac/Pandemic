e57adc1cd93a28f4482bf8bdfe8b3003af35e103
Chris Loew <cloew123@gmail.com>
2013-07-10 11:24:10 -0600

    Finished tests of shouldOutbreak

--- src/Level/City/Test/city_infection_delegate_test.py
+++ src/Level/City/Test/city_infection_delegate_test.py
@@ -13,14 +13,20 @@ class shouldOutbreak(unittest.TestCase):
         """ Build the *** for the test """
         self.infectionDelegate = BuildCityInfectionDelegate()
         
-    def noOutbreak_NewDisease(self):
+    def noOutbreak(self):
         """ Test that no outbreak should happen """
         disease = Disease()
         assert disease not in self.infectionDelegate.city.diseaseCounts, "City should not have the given disease"
-        assert not self.infectionDelegate.shouldOutbreak(CityInfectionDelegate.MAX_INFECTIONS_PER_DISEASE-1, disease), "Should not outbreak when infected by less than the max infection with a new disease"
+        assert not self.infectionDelegate.shouldOutbreak(CityInfectionDelegate.MAX_INFECTIONS_PER_DISEASE, disease), "Should not outbreak when infected by less than or equal to the max infections"
 
+    def shouldOutbreak(self):
+        """ Test that an outbreak can happen properly """
+        disease = Disease()
+        self.infectionDelegate.city.diseaseCounts[disease] = 1
+        assert self.infectionDelegate.shouldOutbreak(CityInfectionDelegate.MAX_INFECTIONS_PER_DISEASE, disease), "Should outbreak when the disease count goes above the max infections"
+        
 # Collect all test cases in this class
-testcasesShouldOutbreak = ["noOutbreak_NewDisease"]
+testcasesShouldOutbreak = ["noOutbreak", "shouldOutbreak"]
 suiteShouldOutbreak = unittest.TestSuite(map(shouldOutbreak, testcasesShouldOutbreak))
 
 ##########################################################
