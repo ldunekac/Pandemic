923ee9bdc9810b2f96a394aba0b3fb97f954ed00
Chris Loew <cloew123@gmail.com>
2013-07-09 22:36:16 -0600

    Outbreak test now uses the Test Helper

--- src/Level/Disease/Outbreak/Test/outbreak_test.py
+++ src/Level/Disease/Outbreak/Test/outbreak_test.py
@@ -4,58 +4,55 @@ from Level.city import City
 from Level.Disease.disease import Disease
 from Level.Disease.Outbreak.outbreak import Outbreak
 
+from Test.test_helper import GetCityList
+
 class breakout(unittest.TestCase):
     """ Test cases of breakout """
     
     def  setUp(self):
-        """ Build the *** for the test """
-        self.disease = Disease()
-        
-        self.city1 = City("Blah1", self.disease)
-        self.city2 = City("Blah2", self.disease)
-        self.city3 = City("Blah3", self.disease)
-        self.city1.addAdjacentCity(self.city2)
-        self.city1.addAdjacentCity(self.city3)
+        """ Build the Cities and Outbreak for the test """
+        self.cities = GetCityList()
+        self.disease = self.cities[0].disease
         
         self.citiesHitByOutbreak = set()
-        self.outbreak = Outbreak(self.city1, self.disease, self.citiesHitByOutbreak)
+        self.outbreak = Outbreak(self.cities[0], self.disease, self.citiesHitByOutbreak)
         
     def infect(self):
         """ Test that the adjacent cities are infected """
-        assert len(self.city2.diseaseCounts) == 0, "City 2 should not be infected at all"
-        assert len(self.city3.diseaseCounts) == 0, "City 3 should not be infected at all"
+        for city in self.outbreak.startingCity.adjacentCities:
+            assert len(city.diseaseCounts) == 0, "City should not be infected at all"
         
         self.outbreak.breakout()
         
-        assert self.city2.diseaseCounts[self.disease] == 1, "City 2 should have been infected once"
-        assert self.city3.diseaseCounts[self.disease] == 1, "City 3 should have been infected once"
+        for city in self.outbreak.startingCity.adjacentCities:
+            assert len(city.diseaseCounts) == 1, "City should have been infected once"
         
     def multiDisease(self):
         """ Test that the adjacent cities are infected with the outbreak disease """
         disease2 = Disease()
-        self.outbreak = Outbreak(self.city1, disease2, self.citiesHitByOutbreak)
+        self.outbreak = Outbreak(self.cities[0], disease2, self.citiesHitByOutbreak)
         self.outbreak.breakout()
         
-        assert self.city2.disease not in self.city2.diseaseCounts, "City 2 should not be infected with its own disease"
-        assert self.city3.disease not in self.city3.diseaseCounts, "City 3 should not be infected with its own disease"
-        assert self.city2.diseaseCounts[disease2] == 1, "City 2 should have been infected once by Disease 2"
-        assert self.city3.diseaseCounts[disease2] == 1, "City 3 should have been infected once by Disease 2"
+        for city in self.outbreak.startingCity.adjacentCities:
+            assert city.disease not in city.diseaseCounts, "City should not be infected with its own disease"
+        
+        for city in self.outbreak.startingCity.adjacentCities:
+            assert city.diseaseCounts[disease2] == 1, "City should have been infected once by Disease 2"
         
     def citiesHitByOutbreak(self):
-        """ Test that the adjacent cities are infected """
+        """ Test that cities hit by the outbreak are added to the 'Hit by Outbreak' list """
         self.outbreak.breakout()
         
-        assert self.city1 in self.citiesHitByOutbreak, "City 1 should be in the list of cities hit by the outbreak"
-        assert self.city2 in self.citiesHitByOutbreak, "City 2 should be in the list of cities hit by the outbreak"
-        assert self.city3 in self.citiesHitByOutbreak, "City 3 should be in the list of cities hit by the outbreak"
+        for city in self.cities:
+            assert city in self.citiesHitByOutbreak, "City should be in the list of cities hit by the outbreak"
         
     def ignoreCitiesAlreadyHit(self):
-        """ Test that the adjacent cities are infected """
-        self.outbreak.citiesHitByOutbreak = set([self.city1, self.city3])
+        """ Test that cities already hit are ignored """
+        self.outbreak.citiesHitByOutbreak = set([self.cities[0], self.cities[2]])
         self.outbreak.breakout()
         
-        assert self.city2.diseaseCounts[self.disease] == 1, "City 2 should have been infected once"
-        assert self.disease not in self.city3.diseaseCounts, "City 3 should have not been infected"
+        assert self.cities[1].diseaseCounts[self.disease] == 1, "City 2 should have been infected once"
+        assert self.disease not in self.cities[2].diseaseCounts, "Ignored City should have not been infected"
 
 # Collect all test cases in this class
 testcasesBreakout = ["infect", "multiDisease", "citiesHitByOutbreak", "ignoreCitiesAlreadyHit"]
---
+++ src/Test/test_helper.py
@@ -0,0 +1,14 @@
+from Level.city import City
+from Level.Disease.disease import Disease
+
+def GetCityList():
+    """ Returns a list of cities """
+    disease = Disease()
+        
+    city1 = City("Blah1", disease)
+    city2 = City("Blah2", disease)
+    city3 = City("Blah3", disease)
+    city1.addAdjacentCity(city2)
+    city1.addAdjacentCity(city3)
+    
+    return [city1, city2, city3]
