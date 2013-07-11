123cbd9a9495045e8c98093f435fd12464b451b4
Chris Loew <cloew123@gmail.com>
2013-07-10 22:02:46 -0600

    Added the level settings as a singleton

---
+++ src/Level/level_settings.py
@@ -0,0 +1,15 @@
+
+class LevelSettings:
+    """ Container for all level variable settings """
+    
+    def __init__(self):
+        """ Initialize the standard level settings """
+        self.MAX_INFECTIONS_PER_DISEASE = 24
+        self.MAX_INFECTIONS_PER_CITY = 3
+        
+        # This is where we would add all Level Customization
+        # Such as # of diseases
+        # Ability to reshuffle the player deck to continue the game
+        # Extended Infection Rates and so forth
+        
+TheLevelSettings = LevelSettings()
